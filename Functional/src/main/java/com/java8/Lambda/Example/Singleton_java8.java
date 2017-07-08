94d89149a530b05413222eb52fe11a9bd3b146e4
asim <asimarunava@gmail.com>
2017-07-08 15:56:29 +0530

    few changes to Singleton class

--- Functional/src/main/java/com/java8/Lambda/Example/Singleton_java8.java
+++ Functional/src/main/java/com/java8/Lambda/Example/Singleton_java8.java
@@ -9,12 +9,12 @@ public static void main(String[] args) {
 	}
 }
 
-
+//Creating Singleton Using Lambda
 class Resource{
 	
 	private Resource() {System.out.println("Created....");}
 		public Resource op1(){System.out.println("op1"); return this;}
-		public Resource op2(){System.out.println("op1"); return this;}
+		public Resource op2(){System.out.println("op2"); return this;}
 		public static void use(Consumer<Resource> block){
 			Resource reource = new Resource();
 			block.accept(reource);
