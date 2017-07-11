25295552a4675b666305e707520310b0672645e0
Asim Sahoo <asahoo6@sapient.com>
2017-07-11 17:34:25 +0530

    Added few files

---
+++ Functional/src/main/java/com/functional/practice/Static_Interface_Method.java
@@ -0,0 +1,17 @@
+package com.functional.practice;
+
+
+// You can Public static methods to Interface.
+interface Util {
+	  public static int numberOfCores() {
+	    return Runtime.getRuntime().availableProcessors();
+	  }
+	}
+
+	public class Static_Interface_Method {
+	  public static void main(String[] args) {
+	    System.out.println(Util.numberOfCores());
+	  }
+
+	
+	}
---
+++ Functional/src/main/java/com/functional/practice/String_Joining.java
@@ -0,0 +1,21 @@
+package com.functional.practice;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class String_Joining {
+
+	  public static void main(String[] args) throws IOException {
+	    File dir = new File("C:");
+	    
+	    File[] children = dir.listFiles();
+   
+	    System.out.println(
+	      Stream.of(children)
+	            .map(File::getName)
+	            .collect(Collectors.joining(", ")));
+	  }
+
+}
